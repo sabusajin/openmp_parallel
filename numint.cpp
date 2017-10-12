d935e645c623a3d723da847081c80122f5c9f4a6
Sajin Sabu <sajinsabu1905@gmail.com>
2017-10-12 15:15:33 -0700

    Minor revisions to numint

--- numint.cpp
+++ numint.cpp
@@ -29,216 +29,185 @@ float integrate(char* argv[]) {
   int thrds = atoi(argv[6]);
   std::string sched = argv[7];
   int granularity = atoi(argv[8]);
-  float multiplier = (b-a)/(float)n;
+  float multiplier = (b - a) / (float)n;
   float result = 0.0;
 
   omp_set_num_threads(thrds);
 
-  if (sched.compare("static")==0) {
-    if (granularity>0){
-
-        float x = 0.0;
-      #pragma omp parallel for schedule(static, granularity) reduction(+:result)
-      for (int i=0; i<n; i++) {
+  if (sched.compare("static") == 0) {
+    if (granularity > 0) {
+      float x = 0.0;
+#pragma omp parallel for schedule(static, granularity) reduction(+ : result)
+      for (int i = 0; i < n; i++) {
         x = a + ((float)i + 0.5) * multiplier;
         switch (functionid) {
-
           case 1:
-            result = result + f1(x, intensity)*multiplier;
+            result = result + f1(x, intensity) * multiplier;
             break;
           case 2:
-            result = result + f2(x, intensity)*multiplier;
+            result = result + f2(x, intensity) * multiplier;
             break;
           case 3:
-            result = result + f3(x, intensity)*multiplier;
+            result = result + f3(x, intensity) * multiplier;
             break;
           case 4:
-            result = result + f4(x, intensity)*multiplier;
+            result = result + f4(x, intensity) * multiplier;
             break;
           default:
             break;
         }
       }
 
-
+    } else {
+      float x = 0.0;
+#pragma omp parallel for schedule(static) reduction(+ : result)
+      for (int i = 0; i < n; i++) {
+        x = a + ((float)i + 0.5) * multiplier;
+        switch (functionid) {
+          case 1:
+            result = result + f1(x, intensity) * multiplier;
+            break;
+          case 2:
+            result = result + f2(x, intensity) * multiplier;
+            break;
+          case 3:
+            result = result + f3(x, intensity) * multiplier;
+            break;
+          case 4:
+            result = result + f4(x, intensity) * multiplier;
+            break;
+          default:
+            break;
+        }
+      }
     }
-      else {
-
-          float x = 0.0;
-        #pragma omp parallel for schedule(static) reduction(+:result)
-        for (int i=0; i<n; i++) {
-          x = a + ((float)i + 0.5) * multiplier;
-          switch (functionid) {
-
-            case 1:
-              result = result + f1(x, intensity)*multiplier;
-              break;
-            case 2:
-              result = result + f2(x, intensity)*multiplier;
-              break;
-            case 3:
-              result = result + f3(x, intensity)*multiplier;
-              break;
-            case 4:
-              result = result + f4(x, intensity)*multiplier;
-              break;
-            default:
-              break;
-          }
-        }
-
-
-
-
-      }
-  } else if (sched.compare("dynamic")==0) {
-    if (granularity>0){
-
-        float x = 0.0;
-      #pragma omp parallel for schedule(dynamic, granularity) reduction(+:result)
-      for (int i=0; i<n; i++) {
+  } else if (sched.compare("dynamic") == 0) {
+    if (granularity > 0) {
+      float x = 0.0;
+#pragma omp parallel for schedule(dynamic, granularity) reduction(+ : result)
+      for (int i = 0; i < n; i++) {
         x = a + ((float)i + 0.5) * multiplier;
         switch (functionid) {
-
           case 1:
-            result = result + f1(x, intensity)*multiplier;
+            result = result + f1(x, intensity) * multiplier;
             break;
           case 2:
-            result = result + f2(x, intensity)*multiplier;
+            result = result + f2(x, intensity) * multiplier;
             break;
           case 3:
-            result = result + f3(x, intensity)*multiplier;
+            result = result + f3(x, intensity) * multiplier;
             break;
           case 4:
-            result = result + f4(x, intensity)*multiplier;
+            result = result + f4(x, intensity) * multiplier;
             break;
           default:
             break;
         }
       }
 
-
-      }
-      else {
-
-          float x = 0.0;
-        #pragma omp parallel for schedule(dynamic) reduction(+:result)
-        for (int i=0; i<n; i++) {
-          x = a + ((float)i + 0.5) * multiplier;
-          switch (functionid) {
-
-            case 1:
-              result = result + f1(x, intensity)*multiplier;
-              break;
-            case 2:
-              result = result + f2(x, intensity)*multiplier;
-              break;
-            case 3:
-              result = result + f3(x, intensity)*multiplier;
-              break;
-            case 4:
-              result = result + f4(x, intensity)*multiplier;
-              break;
-            default:
-              break;
-          }
-        }
-
-
-
-
-      }
-  }
-  else if (sched.compare("guided")==0) {
-    if (granularity>0){
-
-        float x = 0.0;
-      #pragma omp parallel for schedule(guided, granularity) reduction(+:result)
-      for (int i=0; i<n; i++) {
+    } else {
+      float x = 0.0;
+#pragma omp parallel for schedule(dynamic) reduction(+ : result)
+      for (int i = 0; i < n; i++) {
         x = a + ((float)i + 0.5) * multiplier;
         switch (functionid) {
-
           case 1:
-            result = result + f1(x, intensity)*multiplier;
+            result = result + f1(x, intensity) * multiplier;
             break;
           case 2:
-            result = result + f2(x, intensity)*multiplier;
+            result = result + f2(x, intensity) * multiplier;
             break;
           case 3:
-            result = result + f3(x, intensity)*multiplier;
+            result = result + f3(x, intensity) * multiplier;
             break;
           case 4:
-            result = result + f4(x, intensity)*multiplier;
+            result = result + f4(x, intensity) * multiplier;
+            break;
+          default:
+            break;
+        }
+      }
+    }
+  } else if (sched.compare("guided") == 0) {
+    if (granularity > 0) {
+      float x = 0.0;
+#pragma omp parallel for schedule(guided, granularity) reduction(+ : result)
+      for (int i = 0; i < n; i++) {
+        x = a + ((float)i + 0.5) * multiplier;
+        switch (functionid) {
+          case 1:
+            result = result + f1(x, intensity) * multiplier;
+            break;
+          case 2:
+            result = result + f2(x, intensity) * multiplier;
+            break;
+          case 3:
+            result = result + f3(x, intensity) * multiplier;
+            break;
+          case 4:
+            result = result + f4(x, intensity) * multiplier;
             break;
           default:
             break;
         }
       }
 
-
-
-      }
-      else {
-
-          float x = 0.0;
-        #pragma omp parallel for schedule(guided) reduction(+:result)
-        for (int i=0; i<n; i++) {
-          x = a + ((float)i + 0.5) * multiplier;
-          switch (functionid) {
-
-            case 1:
-              result = result + f1(x, intensity)*multiplier;
-              break;
-            case 2:
-              result = result + f2(x, intensity)*multiplier;
-              break;
-            case 3:
-              result = result + f3(x, intensity)*multiplier;
-              break;
-            case 4:
-              result = result + f4(x, intensity)*multiplier;
-              break;
-            default:
-              break;
-          }
+    } else {
+      float x = 0.0;
+#pragma omp parallel for schedule(guided) reduction(+ : result)
+      for (int i = 0; i < n; i++) {
+        x = a + ((float)i + 0.5) * multiplier;
+        switch (functionid) {
+          case 1:
+            result = result + f1(x, intensity) * multiplier;
+            break;
+          case 2:
+            result = result + f2(x, intensity) * multiplier;
+            break;
+          case 3:
+            result = result + f3(x, intensity) * multiplier;
+            break;
+          case 4:
+            result = result + f4(x, intensity) * multiplier;
+            break;
+          default:
+            break;
         }
-
-
-
       }
+    }
   }
   return result;
-
 }
 
-
-
-int main (int argc, char* argv[]) {
-  //forces openmp to create the threads beforehand
+int main(int argc, char* argv[]) {
+// forces openmp to create the threads beforehand
 #pragma omp parallel
   {
-    int fd = open (argv[0], O_RDONLY);
+    int fd = open(argv[0], O_RDONLY);
     if (fd != -1) {
-      close (fd);
-    }
-    else {
-      std::cerr<<"something is amiss"<<std::endl;
+      close(fd);
+    } else {
+      std::cerr << "something is amiss" << std::endl;
     }
   }
 
   if (argc < 9) {
-    std::cerr<<"Usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity> <nbthreads> <scheduling> <granularity>"<<std::endl;
+    std::cerr << "Usage: " << argv[0]
+              << " <functionid> <a> <b> <n> <intensity> <nbthreads> "
+                 "<scheduling> <granularity>"
+              << std::endl;
     return -1;
   }
-  std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+  std::chrono::time_point<std::chrono::system_clock> start =
+      std::chrono::system_clock::now();
   float result = integrate(argv);
-  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
-  std::chrono::duration<double> elapsed_seconds = end-start;
+  std::chrono::time_point<std::chrono::system_clock> end =
+      std::chrono::system_clock::now();
+  std::chrono::duration<double> elapsed_seconds = end - start;
 
-  std::cout<<result<<std::endl;
-  
-  std::cerr<<elapsed_seconds.count()<<std::endl;
+  std::cout << result << std::endl;
 
+  std::cerr << elapsed_seconds.count() << std::endl;
 
   return 0;
 }
--- numint_seq.cpp
+++ numint_seq.cpp
@@ -3,8 +3,7 @@
 #include <cstdlib>
 #include <chrono>
 
-
-float sequential_integrate (int argc, char* argv[]);
+float sequential_integrate(int argc, char* argv[]);
 
 #ifdef __cplusplus
 extern "C" {
@@ -19,30 +18,30 @@ float f4(float x, int intensity);
 }
 #endif
 
-
-int main (int argc, char* argv[]) {
-
+int main(int argc, char* argv[]) {
   if (argc < 6) {
-    std::cerr<<"usage: "<<argv[0]<<" <functionid> <a> <b> <n> <intensity>"<<std::endl;
+    std::cerr << "usage: " << argv[0] << " <functionid> <a> <b> <n> <intensity>"
+              << std::endl;
     return -1;
   }
 
-  std::chrono::time_point<std::chrono::system_clock> start = std::chrono::system_clock::now();
+  std::chrono::time_point<std::chrono::system_clock> start =
+      std::chrono::system_clock::now();
 
   float integrate = sequential_integrate(argc, argv);
 
-  std::chrono::time_point<std::chrono::system_clock> end = std::chrono::system_clock::now();
+  std::chrono::time_point<std::chrono::system_clock> end =
+      std::chrono::system_clock::now();
 
-  std::chrono::duration<double> elapsed_seconds = end-start;
+  std::chrono::duration<double> elapsed_seconds = end - start;
 
-  std::cout<<integrate<<std::endl;
+  std::cout << integrate << std::endl;
 
-  std::cerr<<elapsed_seconds.count()<<std::endl;
+  std::cerr << elapsed_seconds.count() << std::endl;
 
   return 0;
 }
-float sequential_integrate (int argc, char* argv[]) {
-
+float sequential_integrate(int argc, char* argv[]) {
   int functionid = atoi(argv[1]);
   float a = atof(argv[2]);
   float b = atof(argv[3]);
@@ -50,31 +49,28 @@ float sequential_integrate (int argc, char* argv[]) {
   uint32_t intensity = atoi(argv[5]);
 
   float result = 0.0, x = 0.0;
-  float multiplier = (b-a)/(float)n;
+  float multiplier = (b - a) / (float)n;
   int i;
 
-  for (i=0; i<n; i++) {
-
+  for (i = 0; i < n; i++) {
     x = a + ((float)i + 0.5) * multiplier;
 
     switch (functionid) {
-
       case 1:
-        result = result + f1(x, intensity)*multiplier;
+        result = result + f1(x, intensity) * multiplier;
         break;
       case 2:
-        result = result + f2(x, intensity)*multiplier;
+        result = result + f2(x, intensity) * multiplier;
         break;
       case 3:
-        result = result + f3(x, intensity)*multiplier;
+        result = result + f3(x, intensity) * multiplier;
         break;
       case 4:
-        result = result + f4(x, intensity)*multiplier;
+        result = result + f4(x, intensity) * multiplier;
         break;
       default:
         break;
     }
-
   }
   return result;
 }
